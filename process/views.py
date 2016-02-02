e1e6c952afdbfd9e3015b378a452b6405c3d109d
Niaja <nfarve@mit.edu>
2016-02-02 13:12:53 -0500

    trying to make table for data

--- process/views.py
+++ process/views.py
@@ -226,6 +226,6 @@ def stats(request):
             context_instance=RequestContext(request)
             )
 
-def allDailyStats():
+def allDailyStats(request):
     dailyStats = DailyStats.objects.all()
-    return render_to_response('data.html', {'dailyStats': dailyStats}, context_instance=RequestContext(request))
+    return render_to_response('data.html', {'dailyStats': dailyStats}, context_instance=RequestContext(reques))
